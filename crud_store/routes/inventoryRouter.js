1cd2d5c03e7c2018efb9eb78b1da8fb458841cc5
Oscar Calle <ocalle374@gmail.com>
2023-07-03 21:13:25 -0400

    finished

--- crud_store/routes/inventoryRouter.js
+++ crud_store/routes/inventoryRouter.js
@@ -4,61 +4,58 @@ const inventoryRouter = express.Router();
 
 const Inventory = require('../models/inventory.js');
 
-
-
 //GET
 inventoryRouter.get("/", (request, response, next) => {
     Inventory.find()
-    .exec()
-    .then(inventories => {
-        response.status(200).send(inventories)
-    })
-    .catch(error => {
-        response.status(500)
-        return next(error)
-    })
+        .exec()
+        .then(inventories => {
+            response.status(200).send(inventories)
+        })
+        .catch(error => {
+            response.status(500)
+            return next(error)
+        })
 });
 // POST 
 inventoryRouter.post("/", (request, response, next) => {
     const newInventory = new Inventory(request.body)
     newInventory
-    .save()
-    .then(savedInventory => {
-        return response.status(200).send(savedInventory)
-    })
-    .catch(error => {
-        response.status(500)
-        return next(error)
-    })
-    
+        .save()
+        .then(savedInventory => {
+            return response.status(200).send(savedInventory)
+        })
+        .catch(error => {
+            response.status(500)
+            return next(error)
+        })
+
 });
-
 // GET one
-inventoryRouter.get('/:inventoryid', (request, response, next) =>{
-     Inventory.findOne({_id: request.params.inventoryid})
-    .exec()
-    .then(findInventories => {
-if(!findInventories) {
-    return response.status(404).send("Item not found")
-}        
-return response.status(200).send(`Successfully found item ${findInventories.item} from the database`)
-    })
-})
-
+inventoryRouter.get('/:inventoryid', (request, response, next) => {
+    Inventory.findOne({ _id: request.params.inventoryid })
+        .exec()
+        .then(findInventories => {
+            if (!findInventories) {
+                return response.status(404).send("Item not found")
+            }
+            return response.status(200).send(`Successfully found item ${findInventories.item} from the database`)
+        })
+});
+//DELETE
 inventoryRouter.delete("/:inventoryId", (request, response, next) => {
-    Inventory.findOneAndDelete({_id: request.params.inventoryId})
-    .exec()
-    .then(deletedInventory => {
-        if (!deletedInventory){
-            return response.status(404).send("Item not foun d")
-        }
-        return response.status(200).send(`Successfulle deleted item ${deletedItem.item} from the database`)
-    })
-    .catch(error => {
-        response.status(500)
-        return next(error)
-    })
-})
+    Inventory.findOneAndDelete({ _id: request.params.inventoryId })
+        .exec()
+        .then(deletedInventory => {
+            if (!deletedInventory) {
+                return response.status(404).send("Item not found")
+            }
+            return response.status(200).send(`Successfulle deleted item ${deletedInventory.item} from the database`)
+        })
+        .catch(error => {
+            response.status(500)
+            return next(error)
+        })
+});
 
 
 
